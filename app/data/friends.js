75165b6ba84c9daba39521b1a876c3ad6f3f7a97
gitsunitha <sunitha.pillai@gmail.com>
2020-07-07 13:42:54 -0400

    Bug fix for submit button

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,12 +10,12 @@ var friends = [{
     },
     {
         name: "Ami",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ftinyjpg.com%2Fimages%2Fsocial%2Fwebsite.jpg&imgrefurl=https%3A%2F%2Ftinyjpg.com%2F&tbnid=ShQnriFk8AK93M&vet=12ahUKEwjwip27qa3qAhUOUFMKHbJiDO8QMygAegUIARDQAQ..i&docid=MxJRAkFr5rrEwM&w=1020&h=510&itg=1&q=jpg%20images&ved=2ahUKEwjwip27qa3qAhUOUFMKHbJiDO8QMygAegUIARDQAQ",
+        image: "/images/french.jpg",
         scores: [1, 1, 2, 3, 1, 4, 5, 2, 3, 1],
     },
     {
         name: "Amigo",
-        image: "https://media.npr.org/assets/img/2020/04/22/nmah-ahb2019q046153_custom-cef65066750be11c9bcf0f33ebb2a5fd5fef9633-s1500-c85.jpg/",
+        image: "/images/spanish.jpg",
         scores: [4, 5, 4, 2, 3, 4, 3, 5, 3, 4],
     },
     {
